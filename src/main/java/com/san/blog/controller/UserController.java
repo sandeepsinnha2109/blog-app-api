41707db0d63396280eab297cbcf4fc4b5ac323f6
sandeepsinha2109 <shekhar.sandeep2109@gmail.com>
2022-07-07 23:39:31 +0530

    Model mapper changes

--- src/main/java/com/san/blog/controller/UserController.java
+++ src/main/java/com/san/blog/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.san.blog.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.san.blog.payloads.ApiResponse;
